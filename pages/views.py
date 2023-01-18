be255b8d2f2fd576ccd6eabbd26a0e8615b8798a
sirj_ul_islam <siraj@aitomation.com>
2023-01-18 17:50:33 +0500

    add small other changes

--- pages/views.py
+++ pages/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.choices import price_choices, bedroom_choices, state_choices
 
-from listings.models import Listing
+from listings.models import Listing, Society
 from realtors.models import Realtor
 
 def index(request):
-    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    society = Society.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
-        'listings': listings,
+        'societys': society,
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices
