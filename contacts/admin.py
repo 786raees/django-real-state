a0327a98c77018d5d17e87e20524f7f8e20397a6
sirj_ul_islam <siraj@aitomation.com>
2023-01-21 19:37:41 +0500

    team and about tables

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Contact
+from .models import Contact, Team_members, About
 
 class ContactAdmin(admin.ModelAdmin):
   list_display = ('id', 'name', 'listing', 'email', 'contact_date')
@@ -9,3 +9,23 @@ class ContactAdmin(admin.ModelAdmin):
   list_per_page = 25
 
 admin.site.register(Contact, ContactAdmin)
+
+
+class TeamAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'photo_main',)
+  list_display_links = ('id', )
+  list_editable =('name', 'photo_main',)
+  search_fields = ('name',)
+  list_per_page = 25
+
+admin.site.register(Team_members, TeamAdmin)
+
+
+class AboutAdmin(admin.ModelAdmin):
+  list_display = ('id','is_published', 'description','title','photo_main','photo_1','photo_2','photo_3','photo_4','photo_5','photo_6','photo_7','photo_8','photo_9','photo_10')
+  list_display_links = ('id', )
+  list_editable = ('is_published','description','title','photo_main','photo_1','photo_2','photo_3','photo_5','photo_6','photo_7','photo_8','photo_9','photo_10')
+  search_fields = ('title', 'description')
+  list_per_page = 25
+
+admin.site.register(About, AboutAdmin)
--- contacts/models.py
+++ contacts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from tinymce import models as tinymce_models
 
 class Contact(models.Model):
   listing = models.CharField(max_length=200)
@@ -12,3 +13,36 @@ class Contact(models.Model):
   user_id = models.IntegerField(blank=True)
   def __str__(self):
     return self.name
+
+
+
+class Team_members(models.Model):
+  name = models.CharField(max_length=200)
+  photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+  is_published = models.BooleanField(default=True)
+  list_date = models.DateTimeField(default=datetime.now, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True, blank=True)
+  updated_at = models.DateTimeField(auto_now=True, blank=True)
+  def __str__(self):
+    return self.name
+
+class About(models.Model):
+  title = models.CharField(max_length=200)
+  description = tinymce_models.HTMLField(blank=True)
+  photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+  photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_7 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_8 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_9 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  photo_10 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+  is_published = models.BooleanField(default=True)
+  list_date = models.DateTimeField(default=datetime.now, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True, blank=True)
+  updated_at = models.DateTimeField(auto_now=True, blank=True)
+  def __str__(self):
+    return str(self.title) + ' About'
